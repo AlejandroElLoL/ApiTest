76f133226190cdc12120458a5dc3e5a9a829157c
AlejandroElLoL <aledbarrios123@gmail.com>
2025-01-22 01:42:38 -0300

    Update cursos.routes.js

--- src/routes/cursos.routes.js
+++ src/routes/cursos.routes.js
@@ -1,12 +1,12 @@
-import { Router } from 'express';
-import { getCursos, getCurso, createCursos, updateCursos, deleteCursos } from '../controllers/cursos.controller.js'
+const { Router } = require('express');
+const { getCursos, getCurso, createCursos, updateCursos, deleteCursos } = require('../controllers/cursos.controller.js');
 
-const router = Router()
+const router = Router();
 
-router.get("/cursos", getCursos)
-router.get("/cursos/:id", getCurso)
-router.post("/cursos", createCursos)
-router.patch("/cursos/:id", updateCursos)   
-router.delete("/cursos/:id", deleteCursos)
+router.get("/cursos", getCursos);
+router.get("/cursos/:id", getCurso);
+router.post("/cursos", createCursos);
+router.patch("/cursos/:id", updateCursos);
+router.delete("/cursos/:id", deleteCursos);
 
-export default router;
+module.exports = router;
