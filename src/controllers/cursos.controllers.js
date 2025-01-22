151f22828a45cbef79588aabe62b324e65127489
AlejandroElLoL <aledbarrios123@gmail.com>
2025-01-22 01:44:32 -0300

    Update cursos.controllers.js

--- src/controllers/cursos.controllers.js
+++ src/controllers/cursos.controllers.js
@@ -1,39 +1,49 @@
-import { pool } from '../db.js'
+const { pool } = require('../db.js');
 
-export const getCursos = async (req, res) => {
+const getCursos = async (req, res) => {
     try {
-        const [rows] = await pool.query('SELECT * FROM cursos')
-        res.json(rows)
+        const [rows] = await pool.query('SELECT * FROM cursos');
+        res.json(rows);
     } catch (error) {
         return res.status(500).json({
-            mensaje: "Algo salió mal."
-        })
+            mensaje: "Algo salió mal.",
+        });
     }
-}
+};
 
-export const getCurso = async (req, res) => {
+const getCurso = async (req, res) => {
     try {
-        const [rows] = await pool.query('SELECT * FROM cursos WHERE id = ?', [req.params.id])
+        const [rows] = await pool.query('SELECT * FROM cursos WHERE id = ?', [req.params.id]);
 
         if (rows.length <= 0) return res.status(404).json({
-            mensaje: 'Curso no encontrado'
-        })
-   
-        res.json(rows[0])
+            mensaje: 'Curso no encontrado',
+        });
+
+        res.json(rows[0]);
     } catch (error) {
         return res.status(500).json({
-            mensaje: "Algo salió mal."
-        })
+            mensaje: "Algo salió mal.",
+        });
     }
-}
+};
 
-export const createCursos = async (req, res) => {
-    const {nombre, modalidad, extraModalidad, comienzo, diasCursada, extraDiasCursada} = req.body
+const createCursos = async (req, res) => {
+    const { nombre, modalidad, extraModalidad, comienzo, diasCursada, extraDiasCursada } = req.body;
     try {
-        if(nombre == undefined || modalidad == undefined || extraModalidad == undefined || comienzo == undefined || diasCursada == undefined || extraDiasCursada == undefined){
-            res.send("Faltan valores")
-        }else {
-            const [rows] = await pool.query('INSERT INTO cursos (nombre, modalidad, extraModalidad, comienzo, diasCursada, extraDiasCursada) VALUES (?, ?, ?, ?, ?, ?)', [nombre, modalidad, extraModalidad, comienzo, diasCursada, extraDiasCursada])
+        if (
+            nombre === undefined || 
+            modalidad === undefined || 
+            extraModalidad === undefined || 
+            comienzo === undefined || 
+            diasCursada === undefined || 
+            extraDiasCursada === undefined
+        ) {
+            res.send("Faltan valores");
+        } else {
+            const [rows] = await pool.query(
+                'INSERT INTO cursos (nombre, modalidad, extraModalidad, comienzo, diasCursada, extraDiasCursada) VALUES (?, ?, ?, ?, ?, ?)', 
+                [nombre, modalidad, extraModalidad, comienzo, diasCursada, extraDiasCursada]
+            );
             res.send({
                 id: rows.insertId,
                 nombre, 
@@ -41,51 +51,61 @@ export const createCursos = async (req, res) => {
                 extraModalidad, 
                 comienzo, 
                 diasCursada, 
-                extraDiasCursada 
-            })
-        } 
+                extraDiasCursada,
+            });
+        }
     } catch (error) {
         return res.status(500).json({
-            mensaje: "Algo salió mal."
-        })
+            mensaje: "Algo salió mal.",
+        });
     }
-}
+};
 
-export const updateCursos = async (req, res) => {
-    const {id} = req.params
-    const {nombre, modalidad, extraModalidad, comienzo, diasCursada, extraDiasCursada} = req.body
+const updateCursos = async (req, res) => {
+    const { id } = req.params;
+    const { nombre, modalidad, extraModalidad, comienzo, diasCursada, extraDiasCursada } = req.body;
     try {
-        const [result] = await pool.query('UPDATE cursos SET nombre = IFNULL(?, nombre), modalidad = IFNULL(?, modalidad), extraModalidad = IFNULL(?, extraModalidad), comienzo = IFNULL(?, comienzo), diasCursada = IFNULL(?, diasCursada), extraDiasCursada = IFNULL(?, extraDiasCursada) WHERE id = ?', [nombre, modalidad, extraModalidad, comienzo, diasCursada, extraDiasCursada, id])
-        
-        if (result.affectedRows === 0) return res.status(404).json ({
-            mensaje: 'Curso no encontrado'
-        })
+        const [result] = await pool.query(
+            'UPDATE cursos SET nombre = IFNULL(?, nombre), modalidad = IFNULL(?, modalidad), extraModalidad = IFNULL(?, extraModalidad), comienzo = IFNULL(?, comienzo), diasCursada = IFNULL(?, diasCursada), extraDiasCursada = IFNULL(?, extraDiasCursada) WHERE id = ?', 
+            [nombre, modalidad, extraModalidad, comienzo, diasCursada, extraDiasCursada, id]
+        );
 
-        const [rows] = await pool.query('SELECT * FROM cursos WHERE id = ?', [id])
+        if (result.affectedRows === 0) return res.status(404).json({
+            mensaje: 'Curso no encontrado',
+        });
 
-        res.json(rows[0])
-        
+        const [rows] = await pool.query('SELECT * FROM cursos WHERE id = ?', [id]);
+
+        res.json(rows[0]);
     } catch (error) {
         return res.status(500).json({
-            mensaje: "Algo salió mal."
-        })
+            mensaje: "Algo salió mal.",
+        });
     }
-}
+};
 
-export const deleteCursos =  async (req, res) => {
+const deleteCursos = async (req, res) => {
     try {
-        const [result] = await pool.query('DELETE FROM cursos WHERE id = ?', [req.params.id])
+        const [result] = await pool.query('DELETE FROM cursos WHERE id = ?', [req.params.id]);
 
         if (result.affectedRows <= 0) return res.status(404).json({
-            mensaje: 'Curso no encontrado'
-        })
-    
+            mensaje: 'Curso no encontrado',
+        });
+
         res.status(204).json({
-            mensaje : "Curso eliminado"
-        })     
+            mensaje: "Curso eliminado",
+        });
     } catch (error) {
         return res.status(500).json({
-            mensaje: "Algo salió mal."
-        })
+            mensaje: "Algo salió mal.",
+        });
     }
-}
+};
+
+module.exports = {
+    getCursos,
+    getCurso,
+    createCursos,
+    updateCursos,
+    deleteCursos,
+};
