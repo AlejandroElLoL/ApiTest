3302692df73f151d25824a230c6916274486c069
AlejandroElLoL <aledbarrios123@gmail.com>
2025-01-22 01:38:40 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
-import express from 'express'
-import cursosRoutes from './routes/cursos.routes.js'
+const express = require('express');
+const cursosRoutes = require('./routes/cursos.routes.js');
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-app.use('/api',cursosRoutes)
+app.use('/api', cursosRoutes);
 
-app.use((req,res,next) => {
+app.use((req, res, next) => {
     res.status(404).json({
-        mensaje: 'ruta no encontrada'
-    })
-})
+        mensaje: 'ruta no encontrada',
+    });
+});
 
-export default app;
+module.exports = app;
